03cd502ab26909ba6274e97405a999d7dfc21692
Paul Fechner <pj.fechner@gmail.com>
2014-11-11 19:29:58 -0600

    Preliminary movement (copying) to new Android Studio project and addition of greenDAO's SQLite ORM

---
+++ app/src/main/java/src/org/distantshoresmedia/model/BaseDataClass.java
@@ -0,0 +1,38 @@
+package org.distantshoresmedia.model;
+
+/**
+ * Created by Fechner on 11/7/14.
+ */
+public class BaseDataClass {
+
+    private int uid;
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public int getUid() {
+        return uid;
+    }
+
+    private int created;
+    public void setCreated(int created) {
+        this.created = created;
+    }
+    public int getCreated() {
+        return created;
+    }
+
+
+    private int updated;
+    public void setUpdated(int updated) {
+        this.updated = updated;
+    }
+    public int getUpdated() {
+        return updated;
+    }
+
+    public BaseDataClass(int uid, int created, int updated){
+        this.uid = uid;
+        this.created = created;
+        this.updated = updated;
+    }
+}
