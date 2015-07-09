6d9b1a71d0d2a3ac543dd033533255864a8333f0
PeeJWeeJ <pj.fechner@gmail.com>
2015-07-09 18:46:43 -0500

    Saving work on zipping.

--- TranslationKeyboard2.0/app/src/main/java/org/distantshoresmedia/activities/ShareActivity.java
+++ TranslationKeyboard2.0/app/src/main/java/org/distantshoresmedia/activities/ShareActivity.java
@@ -18,7 +18,19 @@ import org.distantshoresmedia.sideloading.SideLoadingDataPreparer;
 import org.distantshoresmedia.translationkeyboard20.R;
 import org.json.JSONArray;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 public class ShareActivity extends ActionBarActivity {
 
@@ -95,6 +107,133 @@ public class ShareActivity extends ActionBarActivity {
 
         String data = ar.toString();
 
+        String zipString = compressData(data);
+        String unCompressedString = deCompressData(zipString);
+        compressText(data);
         Log.i(TAG, "Done");
     }
+
+    private String compressData(String uncompressedData) {
+        String compressedData = null;
+        try {
+            if (uncompressedData.length() > 200) {
+
+                System.out.println("Actual Length:" + uncompressedData.length());
+
+                byte[] originalBytes = uncompressedData.getBytes("UTF-8");
+
+                Deflater deflater = new Deflater();
+                deflater.setInput(originalBytes);
+                deflater.finish();
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buf = new byte[8192];
+                while (!deflater.finished()) {
+                    int byteCount = deflater.deflate(buf);
+                    baos.write(buf, 0, byteCount);
+                }
+                deflater.end();
+
+                byte[] compressedBytes = baos.toByteArray();
+
+                compressedData = new String(compressedBytes, 0, compressedBytes.length);
+
+                System.out.println("Compressed data length:" + compressedBytes.length);
+            }
+        }
+        catch (Exception e) {
+            compressedData = null;
+        }
+        return compressedData;
+    }
+
+    private String deCompressData(String compressedData) {
+
+        byte[] compressedBytes = compressedData.getBytes();
+        String deCompressedData = null;
+        try {
+            if (compressedData.length() > 200) {
+
+                Inflater decompresser = new Inflater();
+                decompresser.setInput(compressedData.getBytes(), 0, compressedBytes.length);
+
+                ByteArrayOutputStream dec = new ByteArrayOutputStream();
+
+                byte[] result = new byte[8192];
+                while (!decompresser.finished()) {
+                    int byteCount = decompresser.inflate(result);
+                    dec.write(result, 0, byteCount);
+                }
+
+                byte[] endResult = dec.toByteArray();
+
+                int resultLength = compressedBytes.length;
+                decompresser.end();
+
+                String outStr = dec.toString("UTF-8");
+                deCompressedData = outStr;
+            }
+        }
+        catch (Exception e) {
+            deCompressedData = null;
+        }
+
+        return deCompressedData;
+    }
+
+    private void compressText(String text){
+
+        try {
+            System.out.println("Actual Length:" + text.length());
+
+            byte[] originalBytes = text.getBytes("UTF-8");
+
+            Deflater deflater = new Deflater();
+            deflater.setInput(originalBytes);
+            deflater.finish();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[8192];
+            while (!deflater.finished()) {
+                int byteCount = deflater.deflate(buf);
+                baos.write(buf, 0, byteCount);
+            }
+            deflater.end();
+
+            byte[] compressedBytes = baos.toByteArray();
+
+            String compressedData = new String(compressedBytes, 0, compressedBytes.length);
+
+            System.out.println("Compressed data length:" + compressedBytes.length);
+
+            //Decompresses the data
+            byte[] theBytes = compressedData.getBytes();
+
+            Inflater decompresser = new Inflater();
+            decompresser.setInput(theBytes, 0, compressedBytes.length);
+
+            ByteArrayOutputStream dec = new ByteArrayOutputStream();
+
+            byte[] result = new byte[8192];
+            while (!decompresser.finished()) {
+                int byteCount = decompresser.inflate(result);
+                dec.write(result, 0, byteCount);
+            }
+
+            byte[] endResult = dec.toByteArray();
+
+            int resultLength = compressedBytes.length;
+            decompresser.end();
+
+            String outStr = dec.toString("UTF-8");
+            System.out.println("Decompressed data length:" + outStr.length());
+        }
+         catch (UnsupportedEncodingException e){
+             e.printStackTrace();
+         }
+        catch (DataFormatException e){
+            e.printStackTrace();
+        }
+    }
+
 }
